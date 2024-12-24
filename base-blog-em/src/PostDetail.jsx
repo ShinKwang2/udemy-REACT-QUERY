5a22a78646e332f044d96b0ccfbd914e4fc7143d
kwang <shinkwang@yonsei.ac.kr>
2024-12-24 09:48:03 +0900

    edu: useMutation for update and delete

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,8 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { fetchComments } from './api';
 import './PostDetail.css';
 
-export function PostDetail({ post }) {
+export function PostDetail({ post, deleteMutation, updateMutation }) {
   // replace with useQuery
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['comments', post.id],
@@ -23,7 +23,26 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <div>
+        <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+        {deleteMutation.isPending && <p className="loading">Deleting the post</p>}
+        {deleteMutation.isError && <p className="error">Error deleting the post: {deleteMutation.error.toString()}</p>}
+        {deleteMutation.isSuccess && <p className="success">Post was (not) deleted</p>}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            updateMutation.mutate(post.id);
+          }}
+        >
+          Update title
+        </button>
+        {updateMutation.isPending && <p className="loading">Update the post title</p>}
+        {updateMutation.isError && (
+          <p className="error">Error update the post title: {updateMutation.error.toString()}</p>
+        )}
+        {updateMutation.isSuccess && <p className="success">Post title was (not) updated</p>}
+      </div>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { fetchPosts, deletePost, updatePost } from './api';
 import { PostDetail } from './PostDetail';
@@ -10,6 +10,16 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => deletePost(postId),
+  });
+  // deleteMutation.mutate
+
+  // updateTitle
+  const updateMutation = useMutation({
+    mutationFn: (postId) => updatePost(postId),
+  });
 
   useEffect(() => {
     if (currentPage < maxPostPage) {
@@ -43,7 +53,15 @@ export function Posts() {
     <>
       <ul>
         {data.map((post) => (
-          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => {
+              setSelectedPost(post);
+              deleteMutation.reset();
+              updateMutation.reset();
+            }}
+          >
             {post.title}
           </li>
         ))}
@@ -68,7 +86,9 @@ export function Posts() {
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && (
+        <PostDetail post={selectedPost} deleteMutation={deleteMutation} updateMutation={updateMutation} />
+      )}
     </>
   );
 }
